ab73b3994878ff83f433a05a6d64cac36be5908b
coco-commander <ict79@naver.com>
2026-04-19 13:38:12 +0900

    Update HelloWorld.sol

--- solidity_study/HelloWorld.sol
+++ solidity_study/HelloWorld.sol
@@ -7,4 +7,11 @@ contract HelloWorld {
     function setGreeting(string memory _newGreeting) public {
         greeting = _newGreeting;
     }
+// 숫자를 저장할 변수 하나 추가!
+uint256 public count = 0;
+
+// 숫자를 1씩 올리는 함수!
+function addCount() public {
+    count = count + 1;
+}
 }
